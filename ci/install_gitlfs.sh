f8c76cf0bfb24deff9e086e762ec159993308006
Philippe Pinard <philippe.pinard@gmail.com>
2016-12-18 10:59:51 +0000

    Fix bug

--- ci/install_gitlfs.sh
+++ ci/install_gitlfs.sh
@@ -5,3 +5,5 @@ wget https://github.com/git-lfs/git-lfs/releases/download/v1.5.3/git-lfs-linux-a
 tar xvfz git-lfs-linux-amd64-1.5.3.tar.gz
 mv git-lfs-1.5.3/git-lfs $HOME/bin/git-lfs
 export PATH=$PATH:$HOME/bin/
+
+git lfs pull
--- ci/install_penelope2014.sh
+++ ci/install_penelope2014.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-unzip fortran/penelope2014.zip
-cd fortran/penelope/fsource
+openssl aes-256-cbc -K $encrypted_8ed1e0a06d33_key -iv $encrypted_8ed1e0a06d33_iv
+  -in fortran/penelope2014.zip.enc -out fortran/penelope2014.zip -d
+
+cd fortran
+unzip penelope2014.zip
+
+cd penelope/fsource
 gfortran material.f -o material
+

20f95441f848e6af40516488d2bdea5f5bd9dadf
Philippe Pinard <philippe.pinard@gmail.com>
2016-12-18 11:18:53 +0000

    Create config

--- ci/install_penelope2014.sh
+++ ci/install_penelope2014.sh
@@ -7,3 +7,9 @@ unzip penelope2014.zip
 cd penelope/fsource
 gfortran material.f -o material
 
+cd $HOME/build/pymontecarlo/pypenelopetools
+echo "[pypenelopetools.material.program:Material2014Program]" >> pypenelopetools.cfg
+echo "name=material" >> pypenelopetools.cfg
+echo "version=2014" >> pypenelopetools.cfg
+echo "executable_path=$HOME/build/pymontecarlo/pypenelopetools/fortran/penelope/fsource/material" >> pypenelopetools.cfg
+echo "pendbase_path=$HOME/build/pymontecarlo/pypenelopetools/fortran/penelope/pendbase/" >> pypenelopetools.cfg

4ed4244937a658b5bf924df9144eb1b23e5426e3
Philippe Pinard <philippe.pinard@gmail.com>
2016-12-18 10:53:46 +0000

    Install git lfs

---
+++ ci/install_gitlfs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p $HOME/bin
+wget https://github.com/git-lfs/git-lfs/releases/download/v1.5.3/git-lfs-linux-amd64-1.5.3.tar.gz
+tar xvfz git-lfs-linux-amd64-1.5.3.tar.gz
+mv git-lfs-1.5.3/git-lfs $HOME/bin/git-lfs
+export PATH=$PATH:$HOME/bin/
---
+++ ci/install_penelope2014.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+unzip fortran/penelope2014.zip
+cd fortran/penelope/fsource
+gfortran material.f -o material

890ef1b265a73eefefc25738928404728a96268f
Philippe Pinard <philippe.pinard@gmail.com>
2016-12-29 18:44:30 +0000

    Rename variable

--- pypenelopetools/penmain/test_input.py
+++ pypenelopetools/penmain/test_input.py
@@ -19,44 +19,44 @@ from pypenelopetools.pengeom.geometry import Geometry
 # Globals and constants variables.
 
 def create_example1_disc():
-    penmain = PenmainInput()
+    input = PenmainInput()
 
-    penmain.TITLE.set('Point source and a homogeneous cylinder.')
-    penmain.SKPAR.set(1)
-    penmain.SENERG.set(40e3)
-    penmain.SPOSIT.set(0.0, 0.0, -0.0001)
-    penmain.SCONE.set(0.0, 0.0, 5.0)
+    input.TITLE.set('Point source and a homogeneous cylinder.')
+    input.SKPAR.set(1)
+    input.SENERG.set(40e3)
+    input.SPOSIT.set(0.0, 0.0, -0.0001)
+    input.SCONE.set(0.0, 0.0, 5.0)
 
-    penmain.materials.add("Cu.mat", 1e3, 1e3, 1e3, 0.05, 0.05, 1e3, 1e3)
+    input.materials.add("Cu.mat", 1e3, 1e3, 1e3, 0.05, 0.05, 1e3, 1e3)
 
-    penmain.GEOMFN.set('disc.geo')
-    penmain.PARINP.add(1, 0.005)
-    penmain.PARINP.add(2, 0.01)
-    penmain.DSMAX.add(1, 1e-4)
+    input.GEOMFN.set('disc.geo')
+    input.PARINP.add(1, 0.005)
+    input.PARINP.add(2, 0.01)
+    input.DSMAX.add(1, 1e-4)
 
-    penmain.IFORCE.add(1, 1, 4, 2000, 0.1, 2.0)
-    penmain.IFORCE.add(1, 1, 5, 200, 0.1, 2.0)
-    penmain.IBRSPL.add(1, 2)
-    penmain.IXRSPL.add(1, 2)
+    input.IFORCE.add(1, 1, 4, 2000, 0.1, 2.0)
+    input.IFORCE.add(1, 1, 5, 200, 0.1, 2.0)
+    input.IBRSPL.add(1, 2)
+    input.IXRSPL.add(1, 2)
 
-    penmain.NBE.set(0, 0, 100)
-    penmain.NBANGL.set(45, 18)
+    input.NBE.set(0, 0, 100)
+    input.NBANGL.set(45, 18)
 
-    penmain.impact_detectors.add(0.0, 0.0, 100, 0, 2, kb=1)
+    input.impact_detectors.add(0.0, 0.0, 100, 0, 2, kb=1)
 
-    penmain.energy_deposition_detectors.add(0, 0, 100, kb=1)
+    input.energy_deposition_detectors.add(0, 0, 100, kb=1)
 
-    penmain.GRIDZ.set(0, 0.005, 100)
-    penmain.GRIDR.set(0.01, 50)
+    input.GRIDZ.set(0, 0.005, 100)
+    input.GRIDR.set(0.01, 50)
 
-    penmain.RESUME.set('dump.dat')
-    penmain.DUMPTO.set('dump.dat')
-    penmain.DUMPP.set(60)
+    input.RESUME.set('dump.dat')
+    input.DUMPTO.set('dump.dat')
+    input.DUMPP.set(60)
 
-    penmain.NSIMSH.set(2e9)
-    penmain.TIME.set(600)
+    input.NSIMSH.set(2e9)
+    input.TIME.set(600)
 
-    return penmain, {}
+    return input, {}
 
 def create_example2_plane():
     # Create materials
@@ -77,35 +77,35 @@ def create_example2_plane():
     index_table = geometry.indexify()
     geometry.to_geo(index_table) # Optional
 
-    # Create penmain
-    penmain = PenmainInput()
+    # Create input
+    input = PenmainInput()
 
-    penmain.TITLE.set('Dose in a water phantom with a spherical impact detector')
-    penmain.SKPAR.set(2)
-    penmain.SENERG.set(3e7)
-    penmain.SPOSIT.set(0.0, 0.0, -25.0)
-    penmain.SCONE.set(0.0, 0.0, 5.0)
+    input.TITLE.set('Dose in a water phantom with a spherical impact detector')
+    input.SKPAR.set(2)
+    input.SENERG.set(3e7)
+    input.SPOSIT.set(0.0, 0.0, -25.0)
+    input.SCONE.set(0.0, 0.0, 5.0)
 
-    penmain.materials.add(material_h2o, 1e5, 1e4, 1e5, 0.05, 0.05, 5e3, 5e3)
+    input.materials.add(material_h2o, 1e5, 1e4, 1e5, 0.05, 0.05, 5e3, 5e3)
 
-    penmain.GEOMFN.set('plane.geo')
+    input.GEOMFN.set('plane.geo')
 
-    penmain.NBE.set(1e5, 3e7, 100)
-    penmain.NBANGL.set(45, 18)
+    input.NBE.set(1e5, 3e7, 100)
+    input.NBANGL.set(45, 18)
 
-    penmain.impact_detectors.add(1e5, 0.0, 100, 0, 2, kb=module_detector)
+    input.impact_detectors.add(1e5, 0.0, 100, 0, 2, kb=module_detector)
 
-    penmain.GRIDZ.set(0, 30.0, 60)
-    penmain.GRIDR.set(30.0, 60.0)
+    input.GRIDZ.set(0, 30.0, 60)
+    input.GRIDR.set(30.0, 60.0)
 
-    penmain.RESUME.set('dump.dat')
-    penmain.DUMPTO.set('dump.dat')
-    penmain.DUMPP.set(60)
+    input.RESUME.set('dump.dat')
+    input.DUMPTO.set('dump.dat')
+    input.DUMPP.set(60)
 
-    penmain.NSIMSH.set(1e7)
-    penmain.TIME.set(2e9)
+    input.NSIMSH.set(1e7)
+    input.TIME.set(2e9)
 
-    return penmain, index_table
+    return input, index_table
 
 class TestPenmainInput(unittest.TestCase):
 
@@ -118,37 +118,37 @@ class TestPenmainInput(unittest.TestCase):
     def tearDown(self):
         unittest.TestCase.tearDown(self)
 
-    def _write_read_penmain(self, penmain, index_table):
+    def _write_read_input(self, input, index_table):
         outfileobj = io.StringIO()
-        penmain.write(outfileobj, index_table)
+        input.write(outfileobj, index_table)
 
         infileobj = io.StringIO(outfileobj.getvalue())
-        outpenmain = PenmainInput()
-        outpenmain.read(infileobj)
+        outinput = PenmainInput()
+        outinput.read(infileobj)
 
         outfileobj.close()
         infileobj.close()
 
-        return outpenmain
+        return outinput
 
-    def _test_example1_disc(self, penmain):
-        kparp, = penmain.SKPAR.get()
+    def _test_example1_disc(self, input):
+        kparp, = input.SKPAR.get()
         self.assertEqual(1, kparp)
 
-        se0, = penmain.SENERG.get()
+        se0, = input.SENERG.get()
         self.assertAlmostEqual(40e3, se0, 5)
 
-        sx0, sy0, sz0 = penmain.SPOSIT.get()
+        sx0, sy0, sz0 = input.SPOSIT.get()
         self.assertAlmostEqual(0.0, sx0, 5)
         self.assertAlmostEqual(0.0, sy0, 5)
         self.assertAlmostEqual(-0.0001, sz0, 5)
 
-        theta, phi, alpha = penmain.SCONE.get()
+        theta, phi, alpha = input.SCONE.get()
         self.assertAlmostEqual(0.0, theta, 5)
         self.assertAlmostEqual(0.0, phi, 5)
         self.assertAlmostEqual(5.0, alpha, 5)
 
-        materials = penmain.materials.get()
+        materials = input.materials.get()
         self.assertEqual(1, len(materials))
 
         filename, eabs1, eabs2, eabs3, c1, c2, wcc, wcr = materials[0]
@@ -161,9 +161,9 @@ class TestPenmainInput(unittest.TestCase):
         self.assertAlmostEqual(1e3, wcc, 5)
         self.assertAlmostEqual(1e3, wcr, 5)
 
-        self.assertEqual('disc.geo', penmain.GEOMFN.get()[0])
+        self.assertEqual('disc.geo', input.GEOMFN.get()[0])
 
-        parinps = penmain.PARINP.get()
+        parinps = input.PARINP.get()
         self.assertEqual(2, len(parinps))
 
         ip, parinp = parinps[0]
@@ -174,14 +174,14 @@ class TestPenmainInput(unittest.TestCase):
         self.assertEqual(2, ip)
         self.assertAlmostEqual(0.01, parinp, 5)
 
-        dsmaxs = penmain.DSMAX.get()
+        dsmaxs = input.DSMAX.get()
         self.assertEqual(1, len(dsmaxs))
 
         kb, dsmax = dsmaxs[0]
         self.assertEqual(1, kb)
         self.assertAlmostEqual(1e-4, dsmax, 5)
 
-        iforces = penmain.IFORCE.get()
+        iforces = input.IFORCE.get()
         self.assertEqual(2, len(iforces))
 
         kb, kpar, icol, forcer, wlow, whig = iforces[0]
@@ -200,30 +200,30 @@ class TestPenmainInput(unittest.TestCase):
         self.assertAlmostEqual(0.1, wlow, 5)
         self.assertAlmostEqual(2.0, whig, 5)
 
-        ibrspls = penmain.IBRSPL.get()
+        ibrspls = input.IBRSPL.get()
         self.assertEqual(1, len(ibrspls))
 
         kb, factor = ibrspls[0]
         self.assertEqual(1, kb)
         self.assertAlmostEqual(2.0, factor, 5)
 
-        ixrspls = penmain.IXRSPL.get()
+        ixrspls = input.IXRSPL.get()
         self.assertEqual(1, len(ixrspls))
 
         kb, factor = ibrspls[0]
         self.assertEqual(1, kb)
         self.assertAlmostEqual(2.0, factor, 5)
 
-        el, eu, nbe = penmain.NBE.get()
+        el, eu, nbe = input.NBE.get()
         self.assertAlmostEqual(0.0, el, 5)
         self.assertAlmostEqual(0.0, eu, 5)
         self.assertEqual(100, nbe)
 
-        nbth, nbph = penmain.NBANGL.get()
+        nbth, nbph = input.NBANGL.get()
         self.assertEqual(45, nbth)
         self.assertEqual(18, nbph)
 
-        impact_detectors = penmain.impact_detectors.get()
+        impact_detectors = input.impact_detectors.get()
         self.assertEqual(1, len(impact_detectors))
 
         (el, eu, nbe, ipsf, idcut,
@@ -244,7 +244,7 @@ class TestPenmainInput(unittest.TestCase):
         self.assertEqual(1, kb[0])
         self.assertEqual(0, len(kpar))
 
-        energy_deposition_detectors = penmain.energy_deposition_detectors.get()
+        energy_deposition_detectors = input.energy_deposition_detectors.get()
         self.assertEqual(1, len(energy_deposition_detectors))
 
         el, eu, nbe, spectrum_filename, kb = energy_deposition_detectors[0]
@@ -254,60 +254,60 @@ class TestPenmainInput(unittest.TestCase):
         self.assertIsNone(spectrum_filename)
         self.assertEqual(1, kb[0])
 
-        zl, zu, ndbz = penmain.GRIDZ.get()
+        zl, zu, ndbz = input.GRIDZ.get()
         self.assertAlmostEqual(0.0, zl, 5)
         self.assertAlmostEqual(0.005, zu, 5)
         self.assertEqual(100, ndbz)
 
-        ru, ndbr = penmain.GRIDR.get()
+        ru, ndbr = input.GRIDR.get()
         self.assertAlmostEqual(0.01, ru, 5)
         self.assertEqual(50, ndbr)
 
-        filename, = penmain.RESUME.get()
+        filename, = input.RESUME.get()
         self.assertEqual('dump.dat', filename)
 
-        filename, = penmain.DUMPTO.get()
+        filename, = input.DUMPTO.get()
         self.assertEqual('dump.dat', filename)
 
-        dumpp, = penmain.DUMPP.get()
+        dumpp, = input.DUMPP.get()
         self.assertAlmostEqual(60.0, dumpp, 5)
 
-        dshn, = penmain.NSIMSH.get()
+        dshn, = input.NSIMSH.get()
         self.assertAlmostEqual(2e9, dshn, 5)
 
-        timea, = penmain.TIME.get()
+        timea, = input.TIME.get()
         self.assertAlmostEqual(600.0, timea, 5)
 
     def test_example1_disc_write(self):
-        penmain, index_table = create_example1_disc()
-        penmain = self._write_read_penmain(penmain, index_table)
-        self._test_example1_disc(penmain)
+        input, index_table = create_example1_disc()
+        input = self._write_read_input(input, index_table)
+        self._test_example1_disc(input)
 
     def test_example1_disc_read(self):
         filepath = os.path.join(self.testdatadir, '1-disc', 'disc.in')
-        penmain = PenmainInput()
+        input = PenmainInput()
         with open(filepath, 'r') as fp:
-            penmain.read(fp)
-        self._test_example1_disc(penmain)
+            input.read(fp)
+        self._test_example1_disc(input)
 
-    def _test_example2_plane(self, penmain):
-        kparp, = penmain.SKPAR.get()
+    def _test_example2_plane(self, input):
+        kparp, = input.SKPAR.get()
         self.assertEqual(2, kparp)
 
-        se0, = penmain.SENERG.get()
+        se0, = input.SENERG.get()
         self.assertAlmostEqual(3e7, se0, 5)
 
-        sx0, sy0, sz0 = penmain.SPOSIT.get()
+        sx0, sy0, sz0 = input.SPOSIT.get()
         self.assertAlmostEqual(0.0, sx0, 5)
         self.assertAlmostEqual(0.0, sy0, 5)
         self.assertAlmostEqual(-25.0, sz0, 5)
 
-        theta, phi, alpha = penmain.SCONE.get()
+        theta, phi, alpha = input.SCONE.get()
         self.assertAlmostEqual(0.0, theta, 5)
         self.assertAlmostEqual(0.0, phi, 5)
         self.assertAlmostEqual(5.0, alpha, 5)
 
-        materials = penmain.materials.get()
+        materials = input.materials.get()
         self.assertEqual(1, len(materials))
 
         filename, eabs1, eabs2, eabs3, c1, c2, wcc, wcr = materials[0]
@@ -320,18 +320,18 @@ class TestPenmainInput(unittest.TestCase):
         self.assertAlmostEqual(5e3, wcc, 5)
         self.assertAlmostEqual(5e3, wcr, 5)
 
-        self.assertEqual('plane.geo', penmain.GEOMFN.get()[0])
+        self.assertEqual('plane.geo', input.GEOMFN.get()[0])
 
-        el, eu, nbe = penmain.NBE.get()
+        el, eu, nbe = input.NBE.get()
         self.assertAlmostEqual(1e5, el, 5)
         self.assertAlmostEqual(3e7, eu, 5)
         self.assertEqual(100, nbe)
 
-        nbth, nbph = penmain.NBANGL.get()
+        nbth, nbph = input.NBANGL.get()
         self.assertEqual(45, nbth)
         self.assertEqual(18, nbph)
 
-        impact_detectors = penmain.impact_detectors.get()
+        impact_detectors = input.impact_detectors.get()
         self.assertEqual(1, len(impact_detectors))
 
         (el, eu, nbe, ipsf, idcut,
@@ -352,41 +352,41 @@ class TestPenmainInput(unittest.TestCase):
         self.assertEqual(1, len(kb))
         self.assertEqual(0, len(kpar))
 
-        zl, zu, ndbz = penmain.GRIDZ.get()
+        zl, zu, ndbz = input.GRIDZ.get()
         self.assertAlmostEqual(0.0, zl, 5)
         self.assertAlmostEqual(30.0, zu, 5)
         self.assertEqual(60, ndbz)
 
-        ru, ndbr = penmain.GRIDR.get()
+        ru, ndbr = input.GRIDR.get()
         self.assertAlmostEqual(30.0, ru, 5)
         self.assertEqual(60, ndbr)
 
-        filename, = penmain.RESUME.get()
+        filename, = input.RESUME.get()
         self.assertEqual('dump.dat', filename)
 
-        filename, = penmain.DUMPTO.get()
+        filename, = input.DUMPTO.get()
         self.assertEqual('dump.dat', filename)
 
-        dumpp, = penmain.DUMPP.get()
+        dumpp, = input.DUMPP.get()
         self.assertAlmostEqual(60.0, dumpp, 5)
 
-        dshn, = penmain.NSIMSH.get()
+        dshn, = input.NSIMSH.get()
         self.assertAlmostEqual(1e7, dshn, 5)
 
-        timea, = penmain.TIME.get()
+        timea, = input.TIME.get()
         self.assertAlmostEqual(2e9, timea, 5)
 
     def test_example2_plane_write(self):
-        penmain, index_table = create_example2_plane()
-        penmain = self._write_read_penmain(penmain, index_table)
-        self._test_example2_plane(penmain)
+        input, index_table = create_example2_plane()
+        input = self._write_read_input(input, index_table)
+        self._test_example2_plane(input)
 
     def test_example2_plane_read(self):
         filepath = os.path.join(self.testdatadir, '2-plane', 'plane.in')
-        penmain = PenmainInput()
+        input = PenmainInput()
         with open(filepath, 'r') as fp:
-            penmain.read(fp)
-        self._test_example2_plane(penmain)
+            input.read(fp)
+        self._test_example2_plane(input)
 
 if __name__ == '__main__': #pragma: no cover
     logging.basicConfig()

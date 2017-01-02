2b020510d84d0139b9d4cb8b28127e5d07249e1b
Philippe Pinard <philippe.pinard@gmail.com>
2017-01-02 12:11:43 +0000

    Add program and unit tests

---
+++ pypenelopetools/pencyl/program.py
@@ -0,0 +1,18 @@
+""""""
+
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules.
+from pypenelopetools.penelope.program import PenelopeMainProgram
+
+# Globals and constants variables.
+
+class _PencylProgram(PenelopeMainProgram):
+
+    NAME = 'pencyl'
+
+class Pencyl2014Program(_PencylProgram):
+
+    VERSION = '2014'
